343315d82e06d3a116716360d7826b3bfda8b7d1
James Murray <93530658+JamesEMurray@users.noreply.github.com>
2023-11-09 14:01:37 +0000

    Circle to Circle Collision added

--- Generic Collider/Generic Collider.cpp
+++ Generic Collider/Generic Collider.cpp
@@ -1,4 +1,4 @@
-/// <summary>
+﻿/// <summary>
 /// Name: James Murray - C00272116
 /// Date: 09/11/23
 /// Programme Purpose: Make a default collider for different collision types
@@ -7,8 +7,14 @@
 
 #include <iostream>
 
+//structs
+struct Circle {
+    short radius = 0;
+    short x; short y;
+};
+
 //Function Initialisers:
-void c2Circle_Circle(); //Circle to Circle Collisions
+void c2Circle_Circle(Circle circle1, Circle circle2); //Circle to Circle Collisions
 void c2Capsule_Circle(); //Capsule to Circle Collisions
 void c2AABB_AABB_Circle(); //Axis-Aligned Bounding Box to Axis-Aligned Bounding Box to Circle Collisions
 void c2Ray_Circle_AABB_Capsule(); //Ray to Circle to Axis-Aligned Bounding Boxes to Capsule Collisions
@@ -29,9 +35,25 @@ int main()
 /// </summary>
 
 //Circle to Circle Collisions
-void c2Circle_Circle() {
+void c2Circle_Circle(Circle circle1, Circle circle2) {
     bool collide = false;
+    short sumOfRadii;
+    short distanceBetween;
+
+    // If the distance is less than or equal to the sum of the radii of the two circles, then the circles are colliding.
+    sumOfRadii = circle1.radius + circle2.radius;
+    //Distance Between: d = √((x_2 - x_1)² + (y_2 - y_1)²)
+    distanceBetween = sqrt((pow(circle2.x - circle1.x, 2) + pow(circle2.y - circle1.y, 2)));
+
+    if (distanceBetween <= sumOfRadii)
+    {
+        collide = true;
+    }
     
+    if (collide == true)
+    {
+    std::cout << "Collision\n";
+    }
 }
 
 //Capsule to Circle Collisions
@@ -43,9 +65,11 @@ void c2Capsule_Circle() {
 //Axis-Aligned Bounding Box to Axis-Aligned Bounding Box to Circle Collisions
 void c2AABB_AABB_Circle() {
     bool collide = false;
+
 }
 
 //Ray to Circle to Axis-Aligned Bounding Boxes to Capsule Collisions
 void c2Ray_Circle_AABB_Capsule() {
     bool collide = false;
+
 }
